d6b3dc32eb72a09938ebea27c3fdd1bac507d75f
Nikhil Kumar Singh <nikhilksingh97@gmail.com>
2016-12-17 11:15:18 +0530

    Replaced html.parser with html5lib parser

--- rss-generator.py
+++ rss-generator.py
@@ -31,7 +31,7 @@ def google_search(query):
     '''
     urls = []
     response = get_results_page(query)
-    soup = BeautifulSoup(response.read(), 'html.parser')
+    soup = BeautifulSoup(response.read(), 'html5lib')  # using advanced html parser
     # Search for all relevant 'a' tags
     for a in soup.select('.r a'):
         parsed_url = urlparse.urlparse(a['href'])
