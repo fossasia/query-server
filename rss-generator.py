e4cbd9ab4ad075d12a8dce2d184019cbfd97cdad
Lakshay Kalbhor <lakshay@Lakshays-MacBook-Pro.local>
2017-01-08 02:07:46 +0530

    Optimised, removed mechanize

--- rss-generator.py
+++ rss-generator.py
@@ -1,8 +1,8 @@
+
+from __future__ import print_function
 import os, json, sys
 import requests
-import mechanize
 from feedgen.feed import FeedGenerator
-import urlparse
 from bs4 import BeautifulSoup
 
 
@@ -26,10 +26,26 @@ def generateFeed(urls, stype):
         fe = fg.add_entry()
         fe.title(url[0])
         fe.link({'href': url[1], 'rel': 'alternate'})
-    print fg.rss_str(pretty=True)
+    print(fg.rss_str(pretty=True))
     ##Write to file
     file_name = 'data/%s.xml'%query
     fg.rss_file(file_name)
+
+def get_bing_page(query):
+    '''
+    Fetches search response from bing.com
+    returns : result page in html
+    '''
+
+    header = {'User-Agent': 
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36"
+    }
+
+    payload = {'q' : query}
+
+    response = requests.get('http://www.bing.com/search', params=payload, headers=header)
+
+    return response
 
 def bing_search(query):
     ''' Search bing for the query and return set of urls
@@ -38,9 +54,9 @@ def bing_search(query):
     '''
     urls = []
     response = get_bing_page(query)
-    soup = BeautifulSoup(response.read(), 'html5lib')
-    # Search for all relevant 'div' tags with having the results
-    for li in soup.findAll('li', attrs = {'class' : ['b_algo']}):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # Search for all relevant 'li' tags with having the results
+    for li in soup.findAll('li', {'class' : 'b_algo'}):
        # search for title
        title = li.h2.text.replace("\n",'').replace("  ","")
        # get anchor tag having the link
@@ -51,6 +67,21 @@ def bing_search(query):
        urls.append(url_entry)
     return urls
 
+def get_duckduckgo_page(query):
+    '''
+    Search query on duckduckgo
+    Returns : Result page in html
+    '''
+
+    header = {'User-Agent': 
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36"
+    }
+
+    payload = {'q' : query}
+    response = requests.get('https://duckduckgo.com/html', headers=header, params=payload)
+
+    return response
+
 def duckduckgo_search(query):
     ''' Search google for the query and return set of urls
     Returns: urls (list)
@@ -58,18 +89,26 @@ def duckduckgo_search(query):
     '''
     urls = []
     response = get_duckduckgo_page(query)
-    soup = BeautifulSoup(response.read(), 'html5lib')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Search for all relevant 'div' tags with having the results
-    for div in soup.findAll('div', attrs = {'class' : ['result', 'results_links', 'results_links_deep', 'web-result']}):
-       # search for title
-       title = div.h2.text.replace("\n",'').replace("  ","")
-       # get anchor tag having the link
-       url = div.h2.a['href']
-       url_entry = [title, url]
-       urls.append(url_entry)
+    # Search for all relevant 'a' tags with having the results
+    for links in soup.findAll('a',{'class':'result__a'}):
+        urls.append([links.getText(), links.get('href')])
+
     return urls
 
+def get_google_page(query):
+    ''' Fetch the google search results page
+    Returns : Results Page
+    '''
+    header = {'User-Agent': 
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36"
+    }
+
+    payload = {'q' : query}
+    response = requests.get('https://www.google.com/search', headers=header, params=payload)
+
+    return response
 
 def google_search(query):
     ''' Search google for the query and return set of urls
@@ -77,62 +116,16 @@ def google_search(query):
             [[Tile1,url1], [Title2, url2],..]
     '''
     urls = []
-    response = get_results_page(query)
-    soup = BeautifulSoup(response.read(), 'html5lib')  # using advanced html parser
-    # Search for all relevant 'a' tags
-    for a in soup.select('.r a'):
-        parsed_url = urlparse.urlparse(a['href'])
-        # Validate url
-        if 'url' in parsed_url.path:
-            urls.append([a.text, str(urlparse.parse_qs(parsed_url.query)['q'][0])])
+    response = get_google_page(query)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # Search for all relevant 'h3' tags
+    for h3 in soup.findAll('h3',{'class':'r'}):
+
+        links = h3.find('a')
+        
+        urls.append([links.getText(),links.get('href')])
+
     return urls
-
-def get_results_page(query):
-    ''' Fetch the google search results page
-    Returns : Results Page
-    '''
-    br = mechanize.Browser()
-    br.set_handle_robots(False) # Google's robot.txt prevents from scrapping
-    br.addheaders = [('User-agent','Mozilla/5.0')]
-    br.open('http://www.google.com/')
-    br.select_form(name='f')
-    br.form['q'] = query
-    return br.submit()
-
-def get_duckduckgo_page(query):
-    """
-    Fetch the duckduckgo search results page
-
-    :param query:   String to be searched on duckduckgo
-    :return:        Result page containing search results
-    """
-    br = mechanize.Browser()
-    br.set_handle_robots(False)  # Google's robot.txt prevents from scrapping
-    br.addheaders = [('User-agent', 'Mozilla/5.0')]
-    br.open('http://www.duckduckgo.com/html/')
-    br.select_form(name='x')
-    br.form['q'] = query
-    return br.submit()
-
-def get_bing_page(query):
-    """
-    Fetch the bing search results page
-
-    :param query:   String to be searched on bing
-    :return:        Result page containing search results
-    """
-    br = mechanize.Browser()
-    br.set_handle_robots(False)  # Google's robot.txt prevents from scrapping
-    br.addheaders = [('User-agent', 'Mozilla/5.0')]
-    br.open('http://www.bing.com/search')
-    formcount = 0
-    for form in br.forms():
-        if str(form.attrs["id"]) == "sb_form":
-            break
-        formcount += 1
-    br.select_form(nr=formcount)
-    br.form['q'] = query
-    return br.submit()
 
 
 def read_in():
@@ -140,12 +133,15 @@ def read_in():
     return json.loads(lines[0])
 
 def main():
+
+    
     global query
     # read command from npm server
     command = read_in()
     # split the command
     stype, query = command.split('~')
-    print query
+    print(query)
+
     if stype == 'g':
         urls = google_search(query)
     elif stype == 'd':
@@ -157,3 +153,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
