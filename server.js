21e6853b02dbf50adbadf50dbf8b8076457d6af3
enigmaeth <enigmaeth@gmail.com>
2017-02-14 19:33:17 +0530

    partially working with cli

---
+++ server.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var spawn = require('child_process').spawn;
+
+var query = '';
+var dataString = "";
+
+var queries = fs.readFileSync('data/query_list.txt').toString().split("\n");
+function search(data) {
+    console.log(data);
+    if (queries.indexOf(data) == -1) {
+        dataString = '';
+        queries.push(data);
+        var myquery = data.toString();
+        myquery = myquery.split('~')[1];
+
+        console.log("        querying -> " + myquery);
+
+        var py = spawn('python', ['rss-generator.py']);
+        py.stdout.on('data', function(data) {
+            dataString += data.toString();
+        });
+        py.stdout.on('end', function() {
+            console.log(dataString);
+            console.log(' saved to file : query-server/data/' + myquery + '.xml');
+        });
+
+        py.stdin.write(JSON.stringify(data));
+        py.stdin.end();
+
+        fs.appendFile('data/query_list.txt', queries[queries.length-1] + '\n', function(err) {
+            if (err) console.log(err);
+        });
+    }
+};
+module.exports = search;
