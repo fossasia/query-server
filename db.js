472e89c697a544da9bb57363484211cb2831b354
enigmaeth <enigmaeth@gmail.com>
2017-02-16 05:29:26 +0530

    Database Connected

--- db.js
+++ db.js
@@ -3,8 +3,14 @@ var assert = require('assert');
 var express = require('express');
 var app = express();
 var searcher = require(__dirname + '/search');
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var spawn = require('child_process').spawn;
 
-
+var query = '';
+var dataString = "";
+var queries = [];
 var url = 'mongodb://localhost:27017/query-server';
 
 MongoClient.connect(url, function(err, db) {
@@ -17,16 +23,46 @@ MongoClient.connect(url, function(err, db) {
     });
 
     app.post('/',function(req, res, next) {
-        searcher(req.query.search,displayResult);
-        function displayResult( error, dataString){
-          if(error)
-              console.log(error);
-          else
-              console.log('****'+dataString+'*****');
-        };
+        var data = req.query.search;
         //console.log(results);
-        //res.send(results);
-    });
+        //var search = function(data) {
+        //console.log(data);
+        if (queries.indexOf(data) == -1) {
+            dataString = '';
+            queries.push(data);
+            var myquery = data.toString();
+            myquery = myquery.split('~')[1];
+            console.log("        querying -> " + myquery);
+            var py = spawn('python', ['rss-generator.py']);
 
-    db.close();
+            py.stdout.on('data', function(data) {
+                dataString += data.toString();
+            });
+
+            py.stdout.on('end', function() {
+                console.log(dataString);
+                var xml_file = __dirname + '/data/' + myquery + '.xml';
+                console.log(' saved to file : ' + xml_file);
+                var dbObject =  { query : myquery , xml : dataString , file : xml_file } ;
+                db.collection('xml_files').save(dbObject, function(err, result){
+                    if (err) return console.log(err);
+                    console.log('saved to database');
+                    res.redirect('/')
+                });
+            });
+
+            py.stdin.write(JSON.stringify(data));
+            py.stdin.end();
+
+            fs.appendFile('data/query_list.txt', queries[queries.length-1] + '\n', function(err) {
+                if (err) console.log(err);
+            });
+        } else {
+            var myquery = data.toString();
+            console.log(" already queried -> " + myquery);
+            console.log(' saved to file : query-server/data/' + myquery + '.xml');
+
+        }
+    });
+    //db.close();
 });
