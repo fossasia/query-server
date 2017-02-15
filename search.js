598300bd2467843100aa2429d7eeee7058bf9044
enigmaeth <enigmaeth@gmail.com>
2017-02-16 01:13:05 +0530

    added xml_file

--- search.js
+++ search.js
@@ -8,31 +8,37 @@ var dataString = "";
 
 var queries = fs.readFileSync('data/query_list.txt').toString().split("\n");
 var search = function(data) {
-    console.log(data);
+    //console.log(data);
     if (queries.indexOf(data) == -1) {
         dataString = '';
         queries.push(data);
         var myquery = data.toString();
         myquery = myquery.split('~')[1];
+
         console.log("        querying -> " + myquery);
+
         var py = spawn('python', ['rss-generator.py']);
         py.stdout.on('data', function(data) {
             dataString += data.toString();
         });
         py.stdout.on('end', function() {
             console.log(dataString);
-            console.log(' saved to file : query-server/data/' + myquery + '.xml');
+            var xml_file = __dirname + 'data/' + myquery + '.xml';
+            console.log(' saved to file : ' + xml_file);
+            return xml_file;
         });
+
         py.stdin.write(JSON.stringify(data));
         py.stdin.end();
+
         fs.appendFile('data/query_list.txt', queries[queries.length-1] + '\n', function(err) {
             if (err) console.log(err);
         });
-    }
-    else {
+    } else {
+        var myquery = data.toString();
         console.log(" already queried -> " + myquery);
         console.log(' saved to file : query-server/data/' + myquery + '.xml');
-        //rl.prompt();
+
     }
 };
 module.exports = search ;
