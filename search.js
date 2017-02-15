c1679f6c0dbf0e9257b7abe3e87ee088a3c871e5
enigmaeth <enigmaeth@gmail.com>
2017-02-16 04:34:11 +0530

    mongoDB addition

--- search.js
+++ search.js
@@ -14,18 +14,17 @@ var search = function(data) {
         queries.push(data);
         var myquery = data.toString();
         myquery = myquery.split('~')[1];
-
         console.log("        querying -> " + myquery);
-
         var py = spawn('python', ['rss-generator.py']);
+
         py.stdout.on('data', function(data) {
             dataString += data.toString();
         });
+
         py.stdout.on('end', function() {
             console.log(dataString);
-            var xml_file = __dirname + 'data/' + myquery + '.xml';
+            var xml_file = __dirname + '/data/' + myquery + '.xml';
             console.log(' saved to file : ' + xml_file);
-            return xml_file;
         });
 
         py.stdin.write(JSON.stringify(data));
