512561dcc9611d53dcdff4e4c437471f8514b883
enigmaeth <enigmaeth@gmail.com>
2017-02-16 17:44:23 +0530

    ejs renderer added

--- db.js
+++ db.js
@@ -8,15 +8,14 @@ var fs = require('fs');
 var util = require('util');
 var spawn = require('child_process').spawn;
 
-var query = '';
-var dataString = "";
-var queries = [];
 var url = 'mongodb://localhost:27017/query-server';
 
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
+
     app.listen(7001);
+    app.set('view engine', 'ejs');
 
     app.get('/', function(req, res) {
         res.sendFile(__dirname + '/index.html');
@@ -28,11 +27,9 @@ MongoClient.connect(url, function(err, db) {
         myquery = myquery.split('~')[1];
         db.collection('xml_files').find({'query': myquery}).count()
             .then(function(numItems) {
-                console.log(numItems); // Use this to debug
                 //callback(numItems); TODO : write everything in this callback function
                 if (numItems == 0  ) {
                     dataString = '';
-                    //queries.push(data);
                     console.log("        querying -> " + myquery);
 
                     var py = spawn('python', ['rss-generator.py']);
@@ -56,17 +53,17 @@ MongoClient.connect(url, function(err, db) {
 
                 } else {
                     console.log(" already queried -> " + myquery);
-                    console.log(' saved to file : query-server/data/' + myquery + '.xml');
+                    res.redirect('/result');
 
                 }
             });
         app.get('/result', function(req, res) {
             db.collection('xml_files').find({ "query" : myquery }).toArray(function(err, results) {
-            console.log(results);
-            // send HTML file populated with quotes here
+                console.log(results);
+                if (err) return console.log(err);
+                res.render(__dirname+'/views/index.ejs', {dbObject: results});
+            });
         });
-        })
-
     });
     //db.close();
 });
