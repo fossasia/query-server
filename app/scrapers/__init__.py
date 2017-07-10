f0cf8de49dd0c9711b3126dfa89d31f5dbe050e1
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-07-10 22:33:57 +0530

    Fixes #63 separate scrapers are built for each search engine

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -4,6 +4,7 @@ from google import Google
 from duckduckgo import Duckduckgo
 from bing import Bing
 from yahoo import Yahoo
+
 scrapers = {
         'g':Google(),
         'b':Bing(),
@@ -22,6 +23,14 @@ def small_test():
 
 def feedgen(query, engine):
     urls = scrapers[engine].results_search(query)
+    if engine == 'g':
+        urls = scrapers['google'].results_search(query)
+    elif engine == 'd':
+        urls = scrapers['duckduckgo'].results_search(query)
+    elif engine == 'y':
+        urls = scrapers['yahoo'].results_search(query)
+    else:
+        urls = scrapers['bing'].results_search(query)
     result = urls
     print(result)
     print(len(result))
