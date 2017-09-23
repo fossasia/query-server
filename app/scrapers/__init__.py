fd726d52f7d9ee10e7f86d5b40c9579e019befe8
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-09-24 03:50:53 +0530

    Added houndCI config file

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -1,24 +1,27 @@
 from __future__ import print_function
-import os, json, sys
+import os
+import json
+import sys
 from google import Google
 from duckduckgo import Duckduckgo
 from bing import Bing
 from yahoo import Yahoo
+from ask import Ask
+
 scrapers = {
-        'g':Google(),
-        'b':Bing(),
-        'y':Yahoo(),
-        'd':Duckduckgo(),
-    }
+    'g': Google(),
+    'b': Bing(),
+    'y': Yahoo(),
+    'd': Duckduckgo(),
+    'a': Ask()
+}
 
 def read_in():
     lines = sys.stdin.readlines()
     return json.loads(lines[0])
 
-
 def small_test():
     assert type(scrapers.google.results_search('fossasia')) is list
-
 
 def feedgen(query, engine):
     urls = scrapers[engine].results_search(query)
