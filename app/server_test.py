427d35ba8fbfbc5380d425d1afde591c71ecee92
cclauss <cclauss@bluewin.ch>
2017-10-09 10:56:48 +0200

    Create server_test.py
    Just one test that always passes.

---
+++ app/server_test.py
@@ -0,0 +1,13 @@
+"""
+# Let's get pytest turned on before we start testing in earnest
+
+from .scrapers import small_test
+
+
+def test_small_test():
+    small_test()
+"""
+
+
+def true_test():
+    assert True, "We have a problem!"
