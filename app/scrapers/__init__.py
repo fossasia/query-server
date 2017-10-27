bf32974a9944b0cf67326027a82c4e7473fed613
cclauss <cclauss@bluewin.ch>
2017-10-27 14:26:46 +0200

    __init__.py implement absolute_import for Python 3

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -1,13 +1,12 @@
-from __future__ import print_function
-import json
-import sys
-from google import Google
-from duckduckgo import Duckduckgo
-from bing import Bing
-from yahoo import Yahoo
-from ask import Ask
-from yandex import Yandex
-from baidu import Baidu
+from __future__ import absolute_import
+
+from .ask import Ask
+from .baidu import Baidu
+from .bing import Bing
+from .duckduckgo import Duckduckgo
+from .google import Google
+from .yahoo import Yahoo
+from .yandex import Yandex
 
 scrapers = {
     'g': Google(),
@@ -20,13 +19,8 @@ scrapers = {
 }
 
 
-def read_in():
-    lines = sys.stdin.readlines()
-    return json.loads(lines[0])
-
-
 def small_test():
-    assert isinstance(scrapers.google.results_search('fossasia'), list)
+    assert isinstance(scrapers['g'].search('fossasia'), list)
 
 
 def feedgen(query, engine, count=10):
