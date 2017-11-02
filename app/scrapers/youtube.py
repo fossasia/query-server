08550e17b64ded8143dca1c3b0d731a0c3eb39b8
Bhavesh Anand <bhaveshanand7@gmail.com>
2017-11-02 16:23:59 +0000

    Initial commit for Youtube support (#291)

---
+++ app/scrapers/youtube.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from generalized import Scraper
+
+
+class Youtube(Scraper):
+    """Scraper class for Youtube"""
+
+    def __init__(self):
+        self.url = 'https://www.youtube.com/results'
+        self.queryKey = 'search_query'
+
+    def parseResponse(self, soup):
+        """ Parse the response and return list of urls
+        Returns: urls (list)
+                [[Tile1,url1], [Title2, url2],..]
+        """
+        urls = []
+        for a in soup.findAll('a'):
+            if a.get('href').startswith('/watch?'):
+                link = 'https://www.youtube.com' + str(a.get('href'))
+                if not a.getText().startswith('\n\n'):
+                    urls.append({'title': a.getText(), 'link': link})
+            else:
+                continue
+
+        print('Youtube parsed: ' + str(urls))
+
+        return urls
