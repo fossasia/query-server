fef7f8ab2385fa27edbdcd8747bd1d57619167b4
bhaveshAn <bhaveshanand7@gmail.com>
2017-12-03 04:47:16 +0530

    Fixes #306 Add ChinaSo support

--- app/server.py
+++ app/server.py
@@ -48,7 +48,7 @@ def search(search_engine):
         engine = search_engine
         if engine not in ('google', 'bing', 'duckduckgo', 'yahoo', 'ask',
                           'yandex', 'ubaidu', 'exalead', 'quora', 'tyoutube',
-                          'parsijoo', 'mojeek'):
+                          'parsijoo', 'mojeek', 'chinaso'):
             err = [404, 'Incorrect search engine', qformat]
             return bad_request(err)
 
