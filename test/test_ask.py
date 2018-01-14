f634e8f51778693bcef701a649d2cc94139850a1
dilraj45 <dilraj.singh850@gmail.com>
2018-01-14 21:45:14 +0530

    Added unit tests for ask.py (#368)

---
+++ test/test_ask.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+
+from app.scrapers import Ask
+
+
+def test_next_start():
+    if 3 != Ask().next_start(2, None):
+        raise AssertionError()
+
+
+def test_parse_response_for_none():
+    html_text = """<div class="PartialSearchResults-noresults">
+         <div class="PartialSearchResults-noresults-body">
+             <p>No results for:</p>
+             <p><b>44754546546545545465465f4654f654654</b></p>
+             <p>Please try again.</p>
+         </div>
+    </div>"""
+    stub_soup = BeautifulSoup(html_text, 'html.parser')
+    resp = Ask().parse_response(stub_soup)
+    if resp:
+        raise AssertionError()
+
+
+def test_parse_response_with_desc():
+    html_div = """<div class="PartialSearchResults-item" data-zen="true">
+        <div class="PartialSearchResults-item-title">
+            <a class="PartialSearchResults-item-title-link result-link"
+             href='mock_url'>mock_title</a>
+        </div>
+        <p class="PartialSearchResults-item-abstract">mock_desc</p>
+        </div>"""
+    stub_soup_div = BeautifulSoup(html_div, 'html.parser')
+    resp = Ask().parse_response(stub_soup_div)
+    expected_resp = [
+        {
+            'link': u'mock_url',
+            'title': u'mock_title',
+            'desc': u'mock_desc'
+        }
+    ]
+    if not resp == expected_resp:
+        raise AssertionError()
+
+
+def test_parse_response_without_desc():
+    html_div = """<div class="PartialSearchResults-item" data-zen="true">
+        <div class="PartialSearchResults-item-title">
+            <a class="PartialSearchResults-item-title-link result-link"
+             href='mock_url'>mock_title</a>
+        </div>
+        </div>"""
+    stub_soup_div = BeautifulSoup(html_div, 'html.parser')
+    resp = Ask().parse_response(stub_soup_div)
+    expected_resp = [
+        {
+            'link': u'mock_url',
+            'title': u'mock_title'
+        }
+    ]
+    if not resp == expected_resp:
+        raise AssertionError()
