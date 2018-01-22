ae59e50719fd60230a75b821b60e534640c21c35
bhaveshAn <bhaveshanand7@gmail.com>
2018-01-22 22:32:05 +0530

    Fixes #462 Add Video search support for Ask

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -42,6 +42,8 @@ def feed_gen(query, engine, count=10, qtype=''):
     engine = old_names.get(engine, engine)
     if engine in ('quora', 'youtube'):
         urls = scrapers[engine].search_without_count(query)
+    elif engine in ('ask',) and qtype == 'vid':
+        urls = scrapers[engine].video_search(query, count, qtype)
     else:
         urls = scrapers[engine].search(query, count, qtype)
     return urls
