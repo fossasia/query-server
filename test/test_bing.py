e16ebfe2fe214bb4e149f0a29f4a0f4b32e3139e
bhaveshAn <bhaveshanand7@gmail.com>
2018-01-24 00:07:45 +0530

    Added tests

--- test/test_bing.py
+++ test/test_bing.py
@@ -16,3 +16,25 @@ def test_parse_response():
         'desc': u'mock_desc'
     }]
     assert resp == expected_resp
+
+
+def test_parse_image_response():
+    html_text = """<a class="iusc" href="mock_url">mock_title</a>"""
+    dummy_soup = BeautifulSoup(html_text, 'html.parser')
+    resp = Bing().parse_image_response(dummy_soup)
+    expected_resp = [{
+        'link': u'mock_url'
+    }]
+    assert resp == expected_resp
+
+
+def test_parse_video_response():
+    html_text = """<a aria-label="mock_title · Duration" class="mc_vtvc_link"
+        href="mock_url"></a>"""
+    dummy_soup = BeautifulSoup(html_text, 'html.parser')
+    resp = Bing().parse_video_response(dummy_soup)
+    expected_resp = [{
+        'title': u'mock_title',
+        'link': u'mock_url .',
+    }]
+    assert resp == expected_resp
