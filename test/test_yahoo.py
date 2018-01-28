4cdffca1b44dabfeb34a5739b4eac9dd14973a28
Vivek Iyer <vivekbalasundaram@gmail.com>
2018-01-28 07:50:24 +0530

    Addresses #321 Fixes #464 Adds image search support for Yahoo (#447)
    * Added Yahoo image search
    
    Fixed travis
    
    Squashed the commits
    
    * Added tests
    
    * Fixed codacy and travis
    
    * Added staticmethod, removed self argument

--- test/test_yahoo.py
+++ test/test_yahoo.py
@@ -14,3 +14,15 @@ def test_parse_response():
     }]
     resp = Yahoo().parse_response(dummy_soup)
     assert resp == expected_resp
+
+
+def test_parse_image_response():
+    html_text = """<li class="ld"><a aria-label="mock_title">
+                <img data-src='mock_url' class='process'/></a></div>"""
+    dummy_soup = BeautifulSoup(html_text, 'html.parser')
+    expected_resp = [{
+        'title': u'mock_title',
+        'link': u'mock_url'
+    }]
+    resp = Yahoo().parse_image_response(dummy_soup)
+    assert resp == expected_resp
