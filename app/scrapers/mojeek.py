9e093708d410b0e0d99644e56f3f97622e7dccbf
Bhavesh Anand <bhaveshanand7@gmail.com>
2018-01-29 23:22:30 +0530

    Fixes #472 #474 #476 Add News search in Baidu, Parsijoo & Mojeek (#486)

--- app/scrapers/mojeek.py
+++ app/scrapers/mojeek.py
@@ -8,6 +8,7 @@ class Mojeek(Scraper):
     def __init__(self):
         Scraper.__init__(self)
         self.url = 'https://www.mojeek.co.uk/search'
+        self.newsURL = 'https://www.mojeek.co.uk/search'
         self.defaultStart = 1
         self.startKey = 's'
         self.name = 'mojeek'
@@ -23,6 +24,26 @@ class Mojeek(Scraper):
             title = a.getText()
             url = a.get('href')
             urls.append({'title': title, 'link': url})
+
+        print('Mojeek parsed: ' + str(urls))
+
+        return urls
+
+    @staticmethod
+    def parse_news_response(soup):
+        """ Parse response and returns the urls
+
+            Returns: urls (list)
+                    [[url1], [url2], ...]
+        """
+        urls = []
+        for a in soup.findAll('a', attrs={'class': 'ob'}):
+            title = a.getText()
+            url = a.get('href')
+            urls.append({
+                'title': title,
+                'link': url
+            })
 
         print('Mojeek parsed: ' + str(urls))
 
--- test/test_baidu.py
+++ test/test_baidu.py
@@ -14,3 +14,16 @@ def test_parse_response():
         'link': u'mock_url'
     }]
     assert resp == expected_resp
+
+
+def test_parse_news_response():
+    html_text = """<h3 class="c-title">
+        <a href="mock_url" target="_blank">mock_title</a>
+        </h3>"""
+    dummy_soup = BeautifulSoup(html_text, 'html.parser')
+    resp = Baidu().parse_news_response(dummy_soup)
+    expected_resp = [{
+        'title': u'mock_title',
+        'link': u'mock_url'
+    }]
+    assert resp == expected_resp
